baaec99ea1da53aac239346d49f1f065d32310b6
Han <hlgwt1314@126.com>
2017-05-02 23:52:02 +0800

    第一次编写

---
+++ test.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author  Han ,
+ * Date  2017/5/2,
+ * Time  23:50,
+ */
+
+/**
+ * Created by PhpStorm.
+ * User: Han
+ * Date: 2017/5/2
+ * Time: 23:50
+ */
+header('Content-Type:text/html;charset=utf-8');
+
+echo "Hello World!";
+echo "I'm GuanRuiYun";
